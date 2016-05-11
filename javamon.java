087700c057e7d2be4b8509d4764d4bfefb4fa6e7
ssarvad8 <ssarvad8@students.d125.org>
2016-05-11 10:35:28 -0500

    Add files via upload

--- javamon.java
+++ javamon.java
@@ -6,36 +6,38 @@ import javax.swing.*;
 public class javamon extends Applet implements ActionListener
 {
     Button button1, button2, button3;
+    private int counter, stage =1;
+    private String counter1, stage1;
 
     final JFrame frame = new JFrame();
     JPanel panel = new JPanel();
-    boolean drawBat = false;
+
     public void paint(Graphics g)
     {
         g.setColor(Color.white);
         g.fillRect(0,0,500,500);     
 
-        g.setColor(Color.yellow);
+        g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+        g.setColor(Color.black);
+        counter1 = "Counter: "+ counter;
+        stage1 = "Stage: " + stage;
+        g.drawString(stage1,25,50);
+        g.drawString(counter1,25,100);
 
-        
-       
+        if (stage == 1)
+        {
+            g.setColor(Color.black);
+
+        }
+
     }
 
     public void init() {
-        button1 = new Button("Stage 1");
+        button1 = new Button("Feed");
         add(button1);
         button1.addActionListener(this);
 
-        button2 = new Button("Stage 2");
-        add(button2);
-        button2.addActionListener(this);
-
-        button3 = new Button("Stage 3");
-        add(button3);
-        button3.addActionListener(this);
-
         JComboBox<String> comboLanguage = new JComboBox<String>();
-        
         Choice types = new Choice();
         types.add("Air");
         types.add("Poison");        
@@ -47,16 +49,22 @@ public class javamon extends Applet implements ActionListener
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button1) 
         {
-            JOptionPane.showMessageDialog(frame.getComponent(0), "Baby Stage");
-        }
-        else if (e.getSource() == button2)
-        {
-            JOptionPane.showMessageDialog(frame.getComponent(0), "Middle Stage");
-        }
-        else if (e.getSource() == button3)
-        {
-            JOptionPane.showMessageDialog(frame.getComponent(0), "Adult Stage");
+            counter++;
+            repaint();
+            if (counter == 20)
+            {
+                stage = 2;
+                repaint();
+            }
+            else if (counter == 120)
+            {
+                stage = 3;
+                repaint();
+            }
+            if (stage == 3)
+            {
+                JOptionPane.showMessageDialog(frame.getComponent(0), "You have reached the final stage!");
+            }
         }
     }
-
 }
