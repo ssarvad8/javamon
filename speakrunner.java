74da6dd796f40a365345f4a414951a2e73463a0a
ssarvad8 <ssarvad8@students.d125.org>
2016-05-11 10:40:21 -0500

    Add files via upload

--- speakrunner.java
+++ speakrunner.java
@@ -4,8 +4,10 @@ public class speakrunner
     {
          air bat = new bat("wingpup", 1, "black");
          electricity duck = new duck("ducking", 1, "yellow");
+         poison cat = new cat("toxikity", 1, "gold");
          
-         System.out.println(bat.toString() + "\n" + bat.phrases("hi"));
-         System.out.println(duck.toString() + "\n" + duck.phrases("quack"));
+         System.out.println(bat.toString() + "\n" + bat.phrases("hi") +"\n");
+         System.out.println(duck.toString() + "\n" + duck.phrases("quack") + "\n");
+         System.out.println(cat.toString() + "\n" + duck.phrases("meow") + "\n");
     }
 }
