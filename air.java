43ca7bc4c805cee1a9394ecca4457eceb0c1d22c
ssarvad8 <ssarvad8@students.d125.org>
2016-05-11 10:06:25 -0500

    Add files via upload

---
+++ air.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class air implements types
+{
+    ArrayList<String> phrases = new ArrayList<String>();
+
+    private String name;
+    private int stage; 
+
+    public air(String name1, int stage1)
+    {
+        name = name1;
+        stage = stage1;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getStage()
+    {
+        return stage;
+    }
+
+    public void setName (String newName)
+    {
+        name = newName;
+    }
+
+    public void setStage (int newStage)
+    {
+        stage = newStage;
+    }
+
+    public String phrases (String phrase)
+    {
+        phrases.add(phrase);
+        return phrase;
+    }
+    
+    public String toString()
+    {
+        return "Stage: " + getStage() + "\n" + "Name: " + getName();
+    }
+}
