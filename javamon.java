07ec188acaf53a5298e112b409ea2407170812b8
ssarvad8 <ssarvad8@students.d125.org>
2016-05-26 18:46:39 -0500

    Add files via upload

--- javamon.java
+++ javamon.java
@@ -14,8 +14,7 @@ public class javamon extends Applet implements ActionListener
 {
     Button button1, button2, button3; //creating buttons
     private int counter, stage;
-    private String counter1, stage1;
-
+    private String counter1, stage1 ,speak;
     final JFrame frame = new JFrame();
     JPanel panel = new JPanel();
 
@@ -36,6 +35,14 @@ public class javamon extends Applet implements ActionListener
         g.drawString(counter1,25,100);
 
         stage = testCreature.getCurrentStage(); //accesses the current stage from the creature class
+        //teaching the creature a few phrases to say
+        testCreature.teach("I love speaking!");
+        testCreature.teach("Hello World");
+        testCreature.teach("I want more food!");
+        testCreature.teach("I love food!");
+        testCreature.teach("Bye!");
+
+        g.drawString(speak, 600,450); //draws the phrase on panel
         String name;
         Image wingpup, chiroptavian, vamperion,  shockling, toxikitty, eleduck, plaguecat, thunderawk, venompanther; //creates image objects
         if (stage == 1)
@@ -45,56 +52,57 @@ public class javamon extends Applet implements ActionListener
                 wingpup = getImage(getCodeBase(),"wingpup.jpg"); //accesses image which is located in the same folder as the code
                 g.drawImage(wingpup,200,200,this); //displays the image
                 testCreature.setName("air"); //calls the method in creature class and sets the type name
-                name = testCreature.getStageName(); //gets the current stage name from creature class
-                g.drawString(name,600,400);
+                name = "Name: " + testCreature.getStageName(); //gets the current stage name from creature class
+                g.drawString(name,600,400); //draws the string name on applet
                 repaint();
             }
             else if (types.getSelectedItem() == "Electricity")
             {
-                shockling = getImage(getCodeBase(),"shockling.jpg");
-                g.drawImage(shockling,200,200,this);
-                testCreature.setName("electricity");
-                name = testCreature.getStageName();
-                g.drawString(name,600,400);
-                repaint();
+                shockling = getImage(getCodeBase(),"shockling.jpg"); //accesses image which is located in the same folder as the code
+                g.drawImage(shockling,200,200,this);  //displays the image
+                testCreature.setName("electricity"); //calls the method in creature class and sets the type name
+                name = "Name: " + testCreature.getStageName(); //gets the current stage name from creature class
+                g.drawString(name,600,400); //draws the string name on applet
+                repaint(); 
             }
             else 
             {
-                toxikitty = getImage(getCodeBase(),"toxikitty.jpg");
-                g.drawImage(toxikitty,200,200,this);
-                testCreature.setName("poison");
-                name = testCreature.getStageName();
-                g.drawString(name,600,400);
+                toxikitty = getImage(getCodeBase(),"toxikitty.jpg"); //accesses image which is located in the same folder as the code
+                g.drawImage(toxikitty,200,200,this);  //displays the image
+                testCreature.setName("poison"); //calls the method in creature class and sets the type name
+                name = "Name: " + testCreature.getStageName(); //gets the current stage name from creature class
+                g.drawString(name,600,400); //draws the string name on applet
                 repaint();
             }
         }
+
         else if (stage == 2)
         {
             if (types.getSelectedItem() == "Air")
             {
-                chiroptavian = getImage(getCodeBase(),"chiroptavian.jpg");
-                g.drawImage(chiroptavian,200,200,this);
-                testCreature.setName("air");
-                name = testCreature.getStageName();
-                g.drawString(name,600,400);
+                chiroptavian = getImage(getCodeBase(),"chiroptavian.jpg"); //accesses image which is located in the same folder as the code
+                g.drawImage(chiroptavian,200,200,this);  //displays the image
+                testCreature.setName("air"); //calls the method in creature class and sets the type name
+                name = "Name: " + testCreature.getStageName(); //gets the current stage name from creature class
+                g.drawString(name,600,400); //draws the string name on applet
                 repaint();
             }
             else if (types.getSelectedItem() == "Electricity")
             {
-                eleduck = getImage(getCodeBase(),"eleduck.jpg");
-                g.drawImage(eleduck,200,200,this);
-                testCreature.setName("electricity");
-                name = testCreature.getStageName();
-                g.drawString(name,600,400);
+                eleduck = getImage(getCodeBase(),"eleduck.jpg"); //accesses image which is located in the same folder as the code
+                g.drawImage(eleduck,200,200,this);  //displays the image
+                testCreature.setName("electricity"); //calls the method in creature class and sets the type name
+                name = "Name: " + testCreature.getStageName(); //gets the current stage name from creature class
+                g.drawString(name,600,400); //draws the string name on applet
                 repaint();
             }
             else 
             {
-                plaguecat = getImage(getCodeBase(),"plaguecat.jpg");
-                g.drawImage(plaguecat,200,200,this);
-                testCreature.setName("poison");
-                name = testCreature.getStageName();
-                g.drawString(name,600,400);
+                plaguecat = getImage(getCodeBase(),"plaguecat.jpg"); //accesses image which is located in the same folder as the code
+                g.drawImage(plaguecat,200,200,this);  //displays the image
+                testCreature.setName("poison"); //calls the method in creature class and sets the type name
+                name = "Name: " + testCreature.getStageName(); //gets the current stage name from creature class
+                g.drawString(name,600,400); //draws the string name on applet
                 repaint();
             }
         }
@@ -102,29 +110,29 @@ public class javamon extends Applet implements ActionListener
         {
             if (types.getSelectedItem() == "Air")
             {
-                vamperion = getImage(getCodeBase(),"vamperion.jpg");
-                g.drawImage(vamperion,200,200,this);
-                testCreature.setName("air");
-                name = testCreature.getStageName();
-                g.drawString(name,600,400);
+                vamperion = getImage(getCodeBase(),"vamperion.jpg"); //accesses image which is located in the same folder as the code
+                g.drawImage(vamperion,200,200,this);  //displays the image
+                testCreature.setName("air"); //calls the method in creature class and sets the type name
+                name = "Name: " + testCreature.getStageName(); //gets the current stage name from creature class
+                g.drawString(name,600,400); //draws the string name on applet
                 repaint();
             }
             else if (types.getSelectedItem() == "Electricity")
             {
-                thunderawk = getImage(getCodeBase(),"thunderawk.jpg");
-                g.drawImage(thunderawk,200,200,this);
-                testCreature.setName("electricity");
-                name = testCreature.getStageName();
-                g.drawString(name,600,400);
+                thunderawk = getImage(getCodeBase(),"thunderawk.jpg"); //accesses image which is located in the same folder as the code
+                g.drawImage(thunderawk,200,200,this);  //displays the image
+                testCreature.setName("electricity"); //calls the method in creature class and sets the type name
+                name = "Name: " + testCreature.getStageName(); //gets the current stage name from creature class
+                g.drawString(name,600,400); //draws the string name on applet
                 repaint();
             }
             else 
             {
-                venompanther = getImage(getCodeBase(),"venompanther.jpg");
-                g.drawImage(venompanther,200,200,this);
-                testCreature.setName("poison");
-                name = testCreature.getStageName();
-                g.drawString(name,600,400);
+                venompanther = getImage(getCodeBase(),"venompanther.jpg"); //accesses image which is located in the same folder as the code
+                g.drawImage(venompanther,200,200,this);  //displays the image
+                testCreature.setName("poison"); //calls the method in creature class and sets the type name
+                name = "Name: " + testCreature.getStageName(); //gets the current stage name from creature class
+                g.drawString(name,600,400); //draws the string name on applet
                 repaint();
             }
         }
@@ -132,7 +140,7 @@ public class javamon extends Applet implements ActionListener
 
     public void init() {
         button1 = new Button("Feed"); //creatues button to feed
-        add(button1);
+        add(button1); //adds button to panel
         button1.addActionListener(this);
         //creating the drop down menu of types of creature
         types.add("Air");
@@ -143,10 +151,12 @@ public class javamon extends Applet implements ActionListener
     }
 
     public void actionPerformed(ActionEvent e) {
+        
         if (e.getSource() == button1) //counter increases everytime the feed button is clicked
         {
             counter++; 
             testCreature.setCurrentStage(counter); //this is where the counter is sent to the creature class to see if the stage can change
+            speak = testCreature.speak(); //stores a phrases in speak to print on panel
         }
         repaint();
     }
